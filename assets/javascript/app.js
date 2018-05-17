7856286f2817b9ca741ec27b23cb95391ab35b7f
fuzzy <donald.jones83@gmail.com>
2018-05-16 23:10:34 -0400

    new imgaes, modals build, styles

---
+++ assets/javascript/app.js
@@ -0,0 +1,24 @@
+
+$( document ).ready(function() {
+
+    $("#portfolio1").click(function() {
+        $("#modal1").modal('show');
+        // console.log("i was clicked");
+    })
+
+    $("#portfolio2").click(function() {
+        $("#modal2").modal('show');
+        // console.log("i was clicked");
+    })
+
+    $("#portfolio3").click(function() {
+        $("#modal3").modal('show');
+        // console.log("i was clicked");
+    })
+
+    $("#portfolio4").click(function() {
+        $("#modal4").modal('show');
+        // console.log("i was clicked");
+    })
+
+});
